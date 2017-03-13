58a5ed44cb0246184ef38a9abc956c9ebbaa4d9e
iambenwhite <iambenwhite@gmail.com>
2017-03-13 20:16:31 +0000

    amends

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ controller.on('bot_channel_join', function (bot, message) {
 //   return bot.reply(message, 'Okay');
 // });
 
-controller.hears('where are you (.*)',['message_received'],function(bot,message) {
+//controller.hears('where are you (.*)',['message_received'],function(bot,message) {
+controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
   var person = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
   return bot.reply(message, person +' is working from home right now.');
 });
