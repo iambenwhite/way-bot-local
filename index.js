b39aa5ffa90a5e69a26952f93628e9b1344c577a
iambenwhite <iambenwhite@gmail.com>
2017-03-13 20:11:21 +0000

    first commit

--- index.js
+++ index.js
@@ -38,11 +38,16 @@ controller.on('bot_channel_join', function (bot, message) {
 //     bot.reply(message, 'Er,I heard... something!');
 // });
 
-//controller.hears('where are you (.*)',['message_received'],function(bot,message) {
-controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
+// controller.hears('where are you (.*)',['message_received'],function(bot,message) {
+// //controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
+//   var person = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
+//   if (person === 'iambenwhite') {
+//     return bot.reply(message, person +' is working from home right now.');
+//   }
+//   return bot.reply(message, 'Okay');
+// });
+
+controller.hears('where are you (.*)',['message_received'],function(bot,message) {
   var person = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
-  if (person === 'iambenwhite') {
-    return bot.reply(message, person +' is working from home right now.');
-  }
-  return bot.reply(message, 'Okay');
+  return bot.reply(message, person +' is working from home right now.');
 });
