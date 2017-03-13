26e45dd0eeef249d8d857c9b1bdacd6d5c149567
Ben White <iambenwhite@gmail.com>
2017-03-13 16:13:57 +0000

    blah

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['where am I', 'way'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
-  bot.reply(message, 'Hi, I am here, do not panic.')
-})
+// controller.hears(['where am I', 'way'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
+//   bot.reply(message, 'Hi, I am here, do not panic.')
+// })
 
 // controller.on('message_received', function(bot, message) {
 //     bot.reply(message, 'Er,I heard... something!');
 // });
 
-controller.hears('where are you',['message_received'],function(bot,message) {
+controller.hears('where are you (.*)',['message_received'],function(bot,message) {
   var person = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
   if (person === 'iambenwhite') {
     return bot.reply(message, ' is working from home right now.');
