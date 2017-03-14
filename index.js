f2dd40ca973a6c0bc39aa52b1c06eb2e2c49cb72
iambenwhite <iambenwhite@gmail.com>
2017-03-14 20:52:35 +0000

    amends

--- index.js
+++ index.js
@@ -27,18 +27,8 @@ if (token) {
   require('beepboop-botkit').start(controller, { debug: true })
 }
 
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
 
-// controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
-//   var person = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
-//   return bot.reply(message, person +' is working from home right now.');
-// });
-
-//dbconnect();
-
-//function dbconnect(){
+//where are you -------------------------
 
 controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
 
@@ -57,8 +47,7 @@ controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_men
 
   connection.connect();
 
-  //var sql = mysql.format("SELECT url FROM Sonic_url WHERE name=?", [person]);
-  var sql = mysql.format("SELECT * from users WHERE username =?", [person]);
+  var sql = mysql.format('SELECT * from users WHERE username =?', [person]);
 
   connection.query(sql, function(err, result, fields) {
   if (!err)
@@ -79,6 +68,43 @@ controller.hears(['where are you (.*)'],['ambient', 'direct_message','direct_men
 
 
 
+//i am here -------------------------
 
+controller.hears(['i am (.*)'],['ambient', 'direct_message','direct_mention','mention'],function(bot,message) {
+
+  var user = message.user;
+  var status = message.match[1];
+
+
+  var mysql = require('mysql');  
+
+  var connection = mysql.createConnection(
+      {
+        host     : '69.90.163.150',
+        user     : 'thewh134_super',
+        password : 'Super01',
+        database : 'thewh134_waybot',
+      }
+  );
+
+  var sql = mysql.format('SELECT * from users WHERE username =? WHERE status =?', [user, status]);
+
+
+  connection.query(sql, function(err, result, fields) {
+    if (!err)
+    {
+      for (var i in result) {
+              var user = result[i];
+              console.log(user.username + ' - ' + user.first_name + ' ' + user.last_name + ' : ' + user.status);
+              bot.reply(message, user.first_name + ' ' + user.last_name + ' is ' + user.status + ' today.');
+          }
+    }
+    else
+      console.log('Error while performing Query.');
+    });
+
+  connection.end();  
+
+});
 
 
