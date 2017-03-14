e10dc63adc6544f50ef7bb2f42aef5f0da7f8b2b
iambenwhite <iambenwhite@gmail.com>
2017-03-14 21:16:53 +0000

    amends

--- index.js
+++ index.js
@@ -77,34 +77,34 @@ controller.hears(['i am (.*)'],['ambient', 'direct_message','direct_mention','me
   console.log(user);
 
 
-  var mysql = require('mysql');  
+  // var mysql = require('mysql');  
 
-  var connection = mysql.createConnection(
-      {
-        host     : '69.90.163.150',
-        user     : 'thewh134_super',
-        password : 'Super01',
-        database : 'thewh134_waybot',
-      }
-  );
+  // var connection = mysql.createConnection(
+  //     {
+  //       host     : '69.90.163.150',
+  //       user     : 'thewh134_super',
+  //       password : 'Super01',
+  //       database : 'thewh134_waybot',
+  //     }
+  // );
 
-  var sql = mysql.format('UPDATE users SET status =? WHERE username =?', [status, user]);
+  // var sql = mysql.format('UPDATE users SET status =? WHERE username =?', [status, user]);
 
 
-  connection.query(sql, function(err, result, fields) {
-    if (!err)
-    {
-      for (var i in result) {
-              var user = result[i];
-              console.log(user.username + ' - ' + user.first_name + ' ' + user.last_name + ' : ' + user.status);
-              bot.reply(message, user.first_name + ' ' + user.last_name + ' is ' + user.status + ' today.');
-          }
-    }
-    else
-      console.log('Error while performing Query.');
-    });
+  // connection.query(sql, function(err, result, fields) {
+  //   if (!err)
+  //   {
+  //     for (var i in result) {
+  //             var user = result[i];
+  //             console.log(user.username + ' - ' + user.first_name + ' ' + user.last_name + ' : ' + user.status);
+  //             bot.reply(message, user.first_name + ' ' + user.last_name + ' is ' + user.status + ' today.');
+  //         }
+  //   }
+  //   else
+  //     console.log('Error while performing Query.');
+  //   });
 
-  connection.end();  
+  // connection.end();  
 
 });
 
